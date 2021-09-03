cbc47c6c92412fc2107e76821ae54a5c842c0b24
daisukeyamanaka <87561867+daisukeyamanaka@users.noreply.github.com>
2021-09-03 09:58:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+//エラー防止？？//
+
+
+$(document).ready(function () {
+  //初期化//
+
+  var $pagination = $("#pagination"),//次のページ//
+    totalRecords = 0,
+    records = [],
+    recPerPage = 0,
+    nextPageToken = "",
+    totalPages = 0;
+    //宣言//
+
+  var API_KEY = "";
+  var search = "";
+  var duration = "any";
+  var order = "relevance";
+  var beforedate = new Date().toISOString();
+  var afterdate = new Date().toISOString();
+  var maxResults = 10;
+
+  $("#beforedate").val(beforedate);
+  $("#afterdate").val(afterdate);
+//変換//
+  $("#beforedate").change(function () {
+    beforedate = new Date(this.val()).toISOString();
+    $("#beforedate").val(beforedate);
+    afterdate = new Date(this.val()).toISOString();
+    $("#afterdate").val(afterdate);
+  });
+
+  $("#afterdate").change(function () {
+    afterdate = new Date(this.val()).toISOString();
+    $("#afterdate").val(afterdate);
+    beforedate = new Date(this.val()).toISOString();
+    $("#beforedate").val(beforedate);
+  });
+//動画時間//
+  $("#duration").change(function () {
+    duration = $(this).children("option:selected").val();
+  });
+  //検索条件//
+
+  $("#order").change(function () {
+    order = $(this).children("option:selected").val();
+  });
+//トピックス//
+  $("#myForm").submit(function (e) {
+    e.preventDefault();
+//検索ボタン id//
+    search = $("#search").val();
+
+    //beforedate = new Date($("#beforedate").val()).toISOString();
+
+    //afterdate = new Date($("#beforedate").val()).toISOString();
+
+    console.log(beforedate);
+
+    //api//
+    API_KEY = "AIzaSyB_eQ10YO1bAjx0AKlazrTStuphTJxWeZs";
+//検索機能//
+    var url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
+          &part=snippet&q=${search}&maxResults=${maxResults}&publishedAfter=${afterdate}&publishedBefore=${beforedate}&order=${order}&videoDuration=${duration}&type=video`;
+//Ｙｏｕｔｕｂｅ//
+    $.ajax({
+      method: "GET",
+      url: url,
+      beforeSend: function () {
+        $("#btn").attr("disabled", true);
+        $("#results").empty();
+      },
+      success: function (data) {
+        console.log(data);
+        $("#btn").attr("disabled", false);
+        displayVideos(data);
+      },
+    });
+  });
+
+  function apply_pagination() {
+    $pagination.twbsPagination({
+      totalPages: totalPages,
+      visiblePages: 6,
+      onPageClick: function (event, page) {
+        console.log(event);
+        displayRecordsIndex = Math.max(page - 1, 0) * recPerPage;
+        endRec = displayRecordsIndex + recPerPage;
+        console.log(displayRecordsIndex + "ssssssssss" + endRec);
+        displayRecords = records.slice(displayRecordsIndex, endRec);
+        generateRecords(recPerPage, nextPageToken);
+      },
+    });
+  }
+
+  $("#search").change(function () {
+    search = $("#search").val();
+  });
+
+  function generateRecords(recPerPage, nextPageToken) {
+    var url2 = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
+      &part=snippet&q=${search}&maxResults=${maxResults}&pageToken=${nextPageToken}&publishedBefore=${beforedate}&publishedAfter=${afterdate}&order=${order}&videoDuration=${duration}&type=video`;
+
+    $.ajax({
+      method: "GET",
+      url: url2,
+      beforeSend: function () {
+        $("#btn").attr("disabled", true);
+        $("#results").empty();
+      },
+      success: function (data) {
+        console.log(data);
+        $("#btn").attr("disabled", false);
+        displayVideos(data);
+      },
+    });
+  }
+//検索結果関数？？//
+  function displayVideos(data) {
+    recPerPage = data.pageInfo.resultsPerPage;
+    nextPageToken = data.nextPageToken;
+    console.log(records);
+    totalRecords = data.pageInfo.totalResults;
+    totalPages = Math.ceil(totalRecords / recPerPage);
+    apply_pagination();
+    $("#search").val("");
+
+    var videoData = "";
+
+    $("#table").show();
+//検索結果//
+    data.items.forEach((item) => {
+      videoData = `
+                      
+                      <tr>
+                      <td>
+                      <a target="_blank" href="https://www.youtube.com/watch?v=${item.id.videoId}">
+                      ${item.snippet.title}</td>
+                      <td>
+                      <img width="200" height="200" src="${item.snippet.thumbnails.high.url}"/>
+                      </td>
+                      <td>
+                      <a target="_blank" href="https://www.youtube.com/channel/${item.snippet.channelId}">${item.snippet.channelTitle}</a>
+                      </td>
+                      </tr>
+  
+                      `;
+
+      $("#results").append(videoData);
+    });
+  }
+});
